c02c3e7e306f3ef32ffabd1687fffa92cfd0b6ee
Mustafa <mustafa31112@gmail.com>
2022-03-08 14:03:02 -0800

    functionality working

---
+++ main.rb
@@ -0,0 +1,16 @@
+# This is the main entrypoint into the program
+# It requires the other files/gems that it needs
+
+
+require './question'
+require './player'
+require './game'
+
+player1 = Player.new
+player2 = Player.new
+players = [player1, player2]
+
+game = Game.new
+game.start(players)
+
+## Your test code can go here
